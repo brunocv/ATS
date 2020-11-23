a12dffeca7691f7520bba2b6c17f77ee11705930
brunocv <brunocveloso7@gmail.com>
2020-11-23 01:01:58 +0000

    java files e pastas

---
+++ criar_pastas.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+mkdir ../Proj_sonar
+for (( counter=0; counter<99; counter++ ))
+do
+mkdir ../Proj_sonar/$counter
+mkdir ../Proj_sonar/$counter/src
+mkdir ../Proj_sonar/$counter/src/main
+mkdir ../Proj_sonar/$counter/src/main/java
+done 
+printf "Pastas criadas\n"
+
+
+for (( counter=0; counter<99; counter++ ))
+do
+cp pom.xml ../Proj_sonar/$counter
+find ../projectsPOO_1920/$counter -type f -name "*.java" -exec cp -n {} ../Proj_sonar/$counter/src/main/java \;
+done
+printf "Ficheiros java nos sítios corretos + pom.xml\n"
+
+
+for (( counter=0, teste=0; counter<99; counter++ ))
+do
+
+done
+printf "pom.xml pronto e $teste \n"
