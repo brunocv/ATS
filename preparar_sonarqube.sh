aa5f9c5edbf2b51f5ec4243b0d6083b9692848e5
brunocv <brunocveloso7@gmail.com>
2020-11-25 09:11:07 +0000

    atualizado

--- preparar_sonarqube.sh
+++ preparar_sonarqube.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+#Mvn compile e mvn package
 if [ "$(uname)" == "Darwin" ]; then
-    printf "Mac top \n"
+	#METE AQUI RAFA
+	printf "Mvn compile e mvn package feitos no Mac.\n"
 elif [ "$(uname)" == "Linux" ]; then
-    printf "Linux top \n"
+	for (( counter=0; counter<5; counter++ ))
+	do
+	if [ $counter != 38 ] && [ $counter != 42 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
+		gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exit; exec bash"
+	fi
+	done
+	printf "Mvn compile e mvn package feitos no Linux.\n"
 fi
 
+sleep 20
+#Executar o SONARQUBE
+if [ "$(uname)" == "Darwin" ]; then
+    	#METE AQUI RAFA
+	
+	printf "A executar sonarqube no Mac.\n"
+elif [ "$(uname)" == "Linux" ]; then
+    	gnome-terminal -- bash -c "cd; ./opt/sonarqube-8.5.0.37579/bin/linux-x86-64/sonar.sh console; exit; exec bash"
+	printf "A executar sonarqube no Linux.\n"
+fi
