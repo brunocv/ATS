e7aa7854ca8927d3b8cd9ccd6b7a7815cf58d92e
brunocv <brunocveloso7@gmail.com>
2020-11-27 17:07:10 +0000

    versão atualizada das pastas

--- criar_pastas.sh
+++ criar_pastas.sh
@@ -1,35 +1,51 @@
 #!/bin/bash
 
+#Criar pastas vazias
 for (( counter=0; counter<99; counter++ ))
 do
 mkdir -p ../Proj_sonar/$counter/src/main/java
 done 
 printf "Pastas criadas\n"
 
+
+#Copiar trabalhos para as pastas criadas anteriormente e copiar a pom também 
 for (( counter=0; counter<99; counter++ ))
 do
 cp pom.xml ../Proj_sonar/$counter
-#VERSÃO ANTERIOR
-#find ../projectsPOO_1920/$counter -type f -name "*.java" -exec cp -n {} ../Proj_sonar/$counter/src/main/java \;
 cp -a ../projectsPOO_1920/$counter/. ../Proj_sonar/$counter/src/main/java
 done
 printf "Ficheiros java nos sítios corretos + pom.xml\n"
 
 
-for (( counter=0; counter<99; counter++ ))
-do
-if [ "$(uname)" == "Darwin" ] && [ $counter != 1 ] && [ $counter != 11 ] && [ $counter != 33 ] && [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 41 ] && [ $counter != 42 ] && [ $counter != 52 ] && [ $counter != 61 ] && [ $counter != 62 ] && [ $counter != 68 ] && [ $counter != 73 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
-	find ../Proj_sonar/$counter -regex ".*\.java"  -exec grep -l  "public[ static]* void main[ ]*[(].*[)]" {} \;| 
-	sed 's|.*/||' | { read NAME ;sed -i "" 's/TESTE/'$NAME'/g' ../Proj_sonar/$counter/pom.xml ;}
-
-elif [ "$(uname)" == "Linux" ] && [ $counter != 1 ] && [ $counter != 11 ] && [ $counter != 33 ] && [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 41 ] && [ $counter != 42 ] && [ $counter != 52 ] && [ $counter != 61 ] && [ $counter != 62 ] && [ $counter != 68 ] && [ $counter != 73 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
-	find ../Proj_sonar/$counter -regex ".*\.java"  -exec grep -l  "public[ static]* void main[ ]*[(].*[)]" {} \;| 
-	sed 's|.*/||' | { read NAME ;sed -i 's/TESTE/'$NAME'/g' ../Proj_sonar/$counter/pom.xml ;}
+#Encontrar a main dos projetos e alterar a pom com tal, por causa do sed é diferente para o MAC e LINUX
+if [ "$(uname)" == "Darwin" ]; then
+	for (( counter=0; counter<99; counter++ ))
+	do
+	
+		if [ $counter != 1 ] && [ $counter != 11 ] && [ $counter != 33 ] && [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 41 ] && [ $counter != 42 ] && [ $counter != 52 ] && [ $counter != 61 ] && [ $counter != 62 ] && [ $counter != 68 ] && [ $counter != 73 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
+	
+			find ../Proj_sonar/$counter -regex ".*\.java"  -exec grep -l  "public[ static]* void main[ ]*[(].*[)]" {} \;| 
+			sed 's|.*/||' | { read NAME ;sed -i "" 's/TESTE/'$NAME'/g' ../Proj_sonar/$counter/pom.xml ;}
+		fi
+	done
+elif [ "$(uname)" == "Linux" ];then
+	for (( counter=0; counter<99; counter++ ))
+	do
+	
+		if [ $counter != 1 ] && [ $counter != 11 ] && [ $counter != 33 ] && [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 41 ] && [ $counter != 42 ] && [ $counter != 52 ] && [ $counter != 61 ] && [ $counter != 62 ] && [ $counter != 68 ] && [ $counter != 73 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
+	
+			find ../Proj_sonar/$counter -regex ".*\.java"  -exec grep -l  "public[ static]* void main[ ]*[(].*[)]" {} \;| 
+			sed 's|.*/||' | { read NAME ;sed -i 's/TESTE/'$NAME'/g' ../Proj_sonar/$counter/pom.xml ;}
+		fi
+	done
 fi
-done
+
+
+
 
 #quem não tem ficheiros fica com TESTE no pom.xml (38,62,75,80), caso do 42 que não tem main mas tem um menu que pode ser main
 
+#Meter mains de projetos com mais que uma main encontrada, por causa do sed tem que de distinguir entre MAC ou LINUX
 if [ "$(uname)" == "Darwin" ]; then
 	sed -i "" 's/TESTE/TrazAquiApp.java/g' ../Proj_sonar/1/pom.xml 
 	sed -i "" 's/TESTE/TrazAquiApp.java/g' ../Proj_sonar/11/pom.xml 
@@ -54,7 +70,7 @@ fi
 printf "pom.xml pronto.\n"
 
 
-
+#Imprimir todas as mains postas nas poms
 for (( counter=0; counter<99; counter++ ))
 do
 printf "$counter   "
--- preparar_sonarqube.sh
+++ preparar_sonarqube.sh
@@ -7,17 +7,17 @@ if [ "$(uname)" == "Darwin" ]; then
 elif [ "$(uname)" == "Linux" ]; then
 	#23 segundos +/- que demora a correr 1 mvn compile+mvn package
 	ativo=0
-	for (( counter=20; counter<30; counter++ ))
+	for (( counter=0; counter<99; counter++ ))
 	do
 		if [ $counter != 38 ] && [ $counter != 42 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
 			#Este é o oficial
-			#gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exit; exec bash"
+			gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exit; exec bash"
 			#Este é o de teste
-			gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exec bash"
+			#gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exec bash"
 			((ativo++))
 			printf "$counter \n"
 		fi
-		if [ $ativo -gt 3 ]; then
+		if [ $ativo -gt 2 ]; then
 			sleep 25
 			ativo=0
 		fi
