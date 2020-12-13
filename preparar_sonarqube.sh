716c99b958960f0fbbc797ca7a805bb840f7c5d7
Rafael Lourenço <rafaelinacio4512@gmail.com>
2020-12-13 23:39:42 +0000

    prepara_sonar Final version

--- preparar_sonarqube.sh
+++ preparar_sonarqube.sh
@@ -1,51 +1,21 @@
 #!/bin/bash
 #mudar  as preferencias do terminal;
 #Preferences->Perfil->Shell-> When the shell exits -> change to "Close the window" if it was closed properly
+#------------------------
 
-#Mvn compile e mvn package
-if [ "$(uname)" == "Darwin" ]; then
-	ativo=0
-	# proj 35 -> chama funções que nao existem
-	for (( counter=0; counter<99 ; counter++ ))
-	do
-		if [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 42 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
-			DIR=$(pwd)
-			osascript -e 'tell application "Terminal" to do script  "cd '${DIR}/../Proj_sonar/${counter}'; mvn compile; mvn package; exit"'
-			#osascript -e 'tell application "Terminal" to do script  "cd '${DIR}/../Proj_sonar/${counter}'; mvn verify;"'
-			((ativo++))
-			printf "$counter \n"
-		fi
-		if [ $ativo -gt 1 ]; then
-			sleep 25
-			ativo=0
-		fi
-	done
-	printf "Mvn compile e mvn package feitos no Mac.\n"
-elif [ "$(uname)" == "Linux" ]; then
-	#23 segundos +/- que demora a correr 1 mvn compile+mvn package
-	ativo=0
-	for (( counter=0; counter<99; counter++ ))
-	do
-		if [ $counter != 35 ] && [ $counter != 38 ] && [ $counter != 42 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
-			#Este é o oficial
-			gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exit; exec bash"
-			#Este é o de teste
-			#gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; exec bash"
-			((ativo++))
-			printf "$counter \n"
-		fi
-		if [ $ativo -gt 2 ]; then
-			sleep 25
-			ativo=0
-		fi
-	done
-	printf "Mvn compile e mvn package feitos no Linux.\n"
+re='^[0-9]+$'
+printf "Insira o numero de projetos a executa(1 a 99)- "
+read num_proj 
+
+if ! [[ $num_proj =~ $re ]] ; then #verifica se é um numero.
+   	echo "error:Escreva um número entre [1..99]" >&2; exit 1
+elif (( num_proj > 0 )) && (( num_proj < 99 )); then 
+	echo "Ira ser executado $num_proj projeto(s)!"
+else
+    echo "Entre 1 e 99!"; exit 1
 fi
-
-
-sleep 20
+#---------
 #Executar o SONARQUBE
-
 if [ "$(uname)" == "Darwin" ]; then
 	osascript -e 'tell application "Terminal" to do script "/opt/sonarqube-8.5.0.37579/bin/macosx-universal-64/sonar.sh console;exit"'
 	printf "A executar sonarqube no Mac.\n"
@@ -53,5 +23,48 @@ elif [ "$(uname)" == "Linux" ]; then
     	gnome-terminal -- bash -c "cd; ./opt/sonarqube-8.5.0.37579/bin/linux-x86-64/sonar.sh console; exit; exec bash"
 	printf "A executar sonarqube no Linux.\n"
 fi
+#espera que o sonarqube execute.
+sleep 50
+
+#token para adicionar projetos ao sonar
+token =$(curl -X POST  -u admin:admin 'http://localhost:9000/api/user_tokens/generate?name=ATS' | sed  's/^.*"token":"\([^"]*\)".*$/\1/' ) 
+#contador para controlo de projetos em execução
+ativo=0 
+if [ "$(uname)" == "Darwin" ]; then
+	for (( counter=0; counter<num_proj ; counter++ ))
+	do
+		if [ $counter != 38 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
+			DIR=$(pwd)
+			osascript -e 'tell application "Terminal" to do script  "cd '${DIR}/../Proj_sonar/${counter}'; mvn compile; mvn package;mvn sonar:sonar -Dsonar.projectName=p'${counter}' -Dsonar.projectKey=p'${counter}' -Dsonar.host.url=http://localhost:9000 -Dsonar.login='${token}';exit"'
+			((ativo++))
+			printf "$counter \n"
+		fi
+		if [ $ativo -gt 3 ]; then
+			sleep 70
+			ativo=0
+		fi
+	done
+	printf "Foram colocados $num_proj projetos no sonarqube!\n"
+elif [ "$(uname)" == "Linux" ]; then
+	#23 segundos +/- que demora a correr 1 mvn compile+mvn package
+	for (( counter=0; counter<num_proj; counter++ ))
+	do
+		if [ $counter != 38 ] && [ $counter != 62 ] && [ $counter != 75 ] && [ $counter != 80 ]; then
+			#Este é o oficial
+			gnome-terminal -- bash -c "cd ../Proj_sonar/$counter; mvn compile; mvn package; mvn sonar:sonar -Dsonar.projectName=p'${counter}' -Dsonar.projectKey=p'${counter}' -Dsonar.host.url=http://localhost:9000 -Dsonar.login='${token}'; exit; exec bash"
+			((ativo++))
+			printf "$counter \n"
+		fi
+		if [ $ativo -gt 3 ]; then
+			sleep 70
+			ativo=0
+		fi
+	done
+	printf "Foram colocados $num_proj projetos no sonarqube!\n"
+fi
+#elimina o token criado.
+curl -X POST  -u admin:admin 'http://localhost:9000/api/user_tokens/revoke?name=ATS'
+
+
 
 
--- script2.sh
+++ script2.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-vul=$(curl -sX GET -u admin:admin ‘http://http://localhost:9000/api/issues/search?projectKeys=ssl-dev&types=VULNERABILITY 42’);
+vul=$(
+
+
+
+#
+curl -sX GET -u admin:admin -v 'http://localhost:9000/api/components/search?qualifiers=TRK' > ola.json);
 bug=$(curl -sX GET -u admin:admin ‘http://www.example.com/api/issues/search?projectKeys=ssl-dev&types=BUG 17’);
 no_vul=$(echo $vul | jq -r .total);
 no_bug=$(echo $bug | jq -r .total);
@@ -9,7 +14,8 @@ echo “Total number of BUGS are $no_bug”
 
 
 #obter os BUG,VULNARIBILIDADES E CODE SMELLS
-http://localhost:9000/api/issues/search?ps=1&projectKey=%3Cproj0%3E&facets=types
+http://localhost:9000/api/issues/search?ps=1&projectKey=p0&facets=types
+http://localhost:9000/api/issues/search?ps=1&project=p0
 
 
 
@@ -18,4 +24,37 @@ http://localhost:9000/api/issues/search?componentRoots=org.codehaus.sonar:sonar
 
 
 #tecnical debt em minutos. (1 dia=8h)
-http://localhost:9000/api/issues/search?ps=1&projectKey=%3Cproj0%3E&facets=types&types=CODE_SMELL
+http://localhost:9000/api/issues/search?ps=1&projectKey=p0&facets=types&types=CODE_SMELL
+
+
+
+
+
+
+
+
+#a lista de projetos compilados
+curl -sX GET -u admin:admin -v 'http://localhost:9000/api/components/search?qualifiers=TRK' > ola.json
+
+
+#ver tokens existentes
+curl -sX GET -u admin:admin -v 'http://localhost:9000/api/user_tokens/search?name=ATS'
+
+
+
+#criar token
+curl -X POST -v -u admin:admin 'http://localhost:9000/api/user_tokens/generate?name=ATS'
+#delete Token
+curl -X POST -v -u admin:admin 'http://localhost:9000/api/user_tokens/revoke?name=ATS'
+
+
+
+
+#ZOna de edicao
+
+
+curl -sX GET -u admin:admin -v 'http://localhost:9000/api/issues/search'
+
+
+
+   
